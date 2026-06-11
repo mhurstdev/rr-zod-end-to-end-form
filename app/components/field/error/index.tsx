9e1d7a88c4a3ce785f749d2fe3346685f32564ca
Martin Hurst <contact@martinhurst.com>
2026-06-11 08:19:00 +0100

    fix: add red error text + make field error styles extensible

--- app/components/field/error/index.tsx
+++ app/components/field/error/index.tsx
@@ -1,15 +1,26 @@
+import { clsx } from 'clsx';
 import { useFieldContext } from '../root';
+import styles from './index.module.css';
 import type * as React from 'react';
 
 type TFieldErrorProps = React.ComponentProps<'p'>;
 
-export const FieldError = ({ children, ...props }: TFieldErrorProps) => {
+export const FieldError = ({
+	children,
+	className,
+	...props
+}: TFieldErrorProps) => {
 	const { errorId, errors } = useFieldContext();
 
 	if (!errors?.length) return null;
 
 	return (
-		<p {...props} id={errorId} role="alert">
+		<p
+			className={clsx(styles.errorText, className)}
+			{...props}
+			id={errorId}
+			role="alert"
+		>
 			{children ?? errors.join(', ')}
 		</p>
 	);
